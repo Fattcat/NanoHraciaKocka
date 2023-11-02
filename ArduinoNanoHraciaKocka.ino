ba3eb4f66f2083b35a049845f754b6686e3d3309
Dominik Hulin <109801079+Fattcat@users.noreply.github.com>
2023-11-02 18:32:23 +0100

    Create ArduinoNanoHraciaKocka.ino

---
+++ ArduinoNanoHraciaKocka.ino
@@ -0,0 +1,37 @@
+int buttonPin = 8;
+int ledPins[] = {2, 3, 4, 5, 6, 7};
+int randomNumber = 0;
+bool displayNumber = false;
+
+void setup() {
+  randomSeed(analogRead(0));
+  pinMode(buttonPin, INPUT_PULLUP);
+  for (int i = 0; i < 6; i++) {
+    pinMode(ledPins[i], OUTPUT);
+    digitalWrite(ledPins[i], LOW); // Vypnutie LED diód pri inicializácii
+  }
+}
+
+void loop() {
+  if (digitalRead(buttonPin) == LOW) {
+    if (!displayNumber) {
+      int randomTime = random(1, 7); // Generovanie náhodného čísla od 1 do 6
+      randomNumber = randomTime;
+
+      for (int i = 0; i < 6; i++) {
+        if (i < randomTime) {
+          analogWrite(ledPins[i], 10); // Zapnutie LED diód na sile 100 (nižší jas)
+        } else {
+          digitalWrite(ledPins[i], LOW); // Vypnutie LED diód
+        }
+      }
+      displayNumber = true;
+    }
+  } else if (displayNumber) {
+    delay(2000); // Diódy zostanú svietiť po vygenerovaní čísla 2 sekundy
+    for (int i = 0; i < 6; i++) {
+      digitalWrite(ledPins[i], LOW); // Vypnutie všetkých LED diód
+    }
+    displayNumber = false;
+  }
+}
