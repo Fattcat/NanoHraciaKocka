35c257b0e1cac1098387431b788791eafa41a87a
Dominik Hulin <109801079+Fattcat@users.noreply.github.com>
2023-11-04 22:02:22 +0100

    Update ArduinoNanoHraciaKocka.ino

--- ArduinoNanoHraciaKocka.ino
+++ ArduinoNanoHraciaKocka.ino
@@ -1,7 +1,7 @@
 int buttonPin = 8;
 int ledPins[] = {2, 3, 4, 5, 6, 7};
 bool buttonPressed = false;
-int brightnessLevel = 10; // Chýbajúci deklarovaný dátový typ a priradená hodnota
+int brightnessLevel = 10;
 
 void setup() {
   randomSeed(analogRead(0));
@@ -15,15 +15,14 @@ void setup() {
 void loop() {
   if (digitalRead(buttonPin) == LOW && !buttonPressed) {
     buttonPressed = true;
-    int randomTime = random(2, 7); // Generovať náhodné číslo od 2 do 6
+    int randomTime = random(2, 7); // Generovať náhodné číslo od 2 do 7
 
     for (int i = 0; i < randomTime; i++) {
       analogWrite(ledPins[i], brightnessLevel); // Zapnúť náhodné diódy s nastavenou intenzitou svietenia
       digitalWrite(ledPins[i], HIGH);
     }
 
-    delay(1000); // Počkať 1 sekundu
-
+    delay(1000);
     for (int i = 0; i < 6; i++) {
       analogWrite(ledPins[i], 0); // Vypnúť všetky diódy nastavením intenzity na 0
     }
